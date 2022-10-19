7e6bfe9571e4b1cc6e0bbe12e15aa38d7babbcdf
QueenOma <uzomachizy@gmail.com>
2022-10-19 03:03:09 -0700

    a program that prints putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main- Check the code
+ * Return: Always 0
+ */
+int main (void)
+{
+	-putchar('-');
+	-putchar('p');
+	-putchar('u');
+	-putchar('t');
+	-putchar('c');
+	-putchar('h');
+	-putchar('a');
+	-putchar('r');
+	-putchar('\n');
+	return (0);
+}
