f405ca1af440ad2f616e61a7a7c0c5a8b4028f4b
QueenOma <uzomachizy@gmail.com>
2022-10-19 17:27:37 -0700

    second

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * print_alphabet_x10(void) - print alphabets 10 times, in lowercase
- */
+ * print_alphabet_x10(void) - print alphabets 10 times, in lowercase.
+ */ 
 void print_alphabet_x10(void)
 {
 	int count = 0;
